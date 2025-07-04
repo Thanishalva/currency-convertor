40f561c189b5fceaabeb1d8a255a32772fc7becc
Thanish Alva <alvathanish5@gmail.com>
2025-07-04 12:54:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,218 @@
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+
+const bothselects=document.querySelectorAll("select");
+const fromselect=bothselects[0];
+const toselect=bothselects[1];
+
+//adding options for each selects with each country codes
+for(let selects of bothselects){
+for(let ccode in countryList){
+  const newoption=document.createElement("option");
+  newoption.value=countryList[ccode];
+  newoption.textContent=countryList[ccode];
+  newoption.id=ccode;
+  selects.append(newoption);
+}
+}
+
+let f1="";
+let t1="";
+const fromimg=document.querySelector("#fromimg");
+const toimg=document.querySelector("#toimg");
+fromselect.addEventListener("change",()=>{
+  f1=fromselect.options[fromselect.selectedIndex].id.toLowerCase();
+  
+  let fval=fromselect.options[fromselect.selectedIndex].value;
+  let img1url=`https://flagsapi.com/${fval}/flat/64.png`
+  fromimg.src=img1url;
+})
+
+toselect.addEventListener("change",()=>{
+  t1=toselect.options[toselect.selectedIndex].id.toLowerCase();
+  let fval=toselect.options[toselect.selectedIndex].value;
+  let img1url=`https://flagsapi.com/${fval}/flat/64.png`
+  toimg.src=img1url;
+  
+})
+
+const textbox=document.querySelector("#amount");
+
+
+const button=document.querySelector("#check");
+let amt=0;
+
+button.addEventListener("click",async ()=>{
+amt=Number(textbox.value);
+let data= await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${f1}.json`);
+let currvalue=await data.json();
+let currencyperunit=Number(currvalue[f1][t1]);
+let tot=amt*currencyperunit;
+
+let text=`${amt} ${f1.toUpperCase()} = ${tot} ${t1.toUpperCase()}`;
+const result=document.querySelector(".result");
+result.textContent=text;
+
+
+
+
+})
